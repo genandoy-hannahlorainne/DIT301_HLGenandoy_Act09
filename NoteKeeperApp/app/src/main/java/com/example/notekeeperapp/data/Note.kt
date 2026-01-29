98e90764fa63c3aaceb0246abdcfc296a76195ce
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2026-01-30 01:11:05 +0800

    Delete NoteKeeperApp directory

--- NoteKeeperApp/app/src/main/java/com/example/notekeeperapp/data/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.example.notekeeperapp.data
-
-data class Note(
-    val id: Long = 0L,
-    val title: String,
-    val content: String,
-    val timestamp: Long = System.currentTimeMillis()
-)
